92797128d4384154977e735d22a2157bff37012b
princess-chaea <dltjdrms320@gmail.com>
2026-03-09 14:03:55 +0900

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // 이 줄을 추가하세요
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
